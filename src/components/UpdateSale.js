ad8610061d87834884de0a1ed70a946c7eb9bf67
Oyekunletomide <Oyekunletomide@gmail.com>
2023-03-25 13:10:44 +0530

    files added

--- src/components/UpdateSale.js
+++ src/components/UpdateSale.js
@@ -47,9 +47,11 @@ const handleChange = (e) => {
     <form onSubmit={handleSubmit} className='flex flex-col items-center mt-10'>
         <input className='w-96 border-2 py-1.5 pl-6 rounded-full' type='text' placeholder='Enter name of the product' value={saleData.name} required={true} name='name' onChange={handleChange} />
         <input className='w-96 border-2 py-1.5 pl-6 mt-6 rounded-full' type='number' placeholder='Enter product amount' defaultValue={saleData.amount}  required={true} name='amount' onChange={handleChange} />
-        <input className='h-10 bg-blue-400 w-24 rounded-full mt-6 text-white font-bold' type='submit' value='Update' onClick={() => window.location.reload()} />
+        <input className='h-10 bg-blue-400 w-24 rounded-full mt-6 text-white font-bold' type='submit' value='Update'  />
     </form>
   )
 }
 
 export default UpdateSale
+
+// onClick={() => window.location.reload()}
